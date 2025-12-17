968fb438a6aa9e657e9407765328a749d799885d
Roua Elyouzbechi <roua.youzbachi@horizon-tech.tn>
2025-12-17 14:06:04 +0100

    fix: allow CORS for Vercel

--- src/main/java/com/library/lab4sring_boot/config/WebConfig.java
+++ src/main/java/com/library/lab4sring_boot/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5176",
-                        "http://localhost:3000") // Vite dev server & Docker
+                .allowedOriginPatterns("*") // Allow Vercel & Localhost
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
